1b44b3c21ac1a2c729f1c7b3e63cd5a403605511
ananyakamboj <ananyakamboj9955@gmail.com>
2022-12-28 02:27:53 -0500

    Update

--- intro.py
+++ intro.py
@@ -1,10 +1,32 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  8 13:38:45 2022
 
-@author: ananyakamboj
-"""
 
-print("namastey duniya")
-#added a comment
+
+#Functions
+
+#defining a function
+def myFunc():
+  print("Hi, I am Ananya")
+
+
+#calling a function
+myFunc()
+
+
+#defining a function
+def sumFunc(a,b):
+  print("Sum is ", a+b)
+#calling a function
+sumFunc(9,7)
+
+#returning a value
+
+def sumReturn(a,b):
+  return a+b
+
+c= sumReturn(8,7)
+print(c)
+
+def printHello():
+  print("hello")
+
+a=10
