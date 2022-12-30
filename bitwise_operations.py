8d95c57d38087d8410f0bd8f1449f64a060ac435
ananyakamboj <ananyakamboj9955@gmail.com>
2022-12-30 16:05:19 -0500

    Bitwise Operators

---
+++ bitwise_operations.py
@@ -0,0 +1,17 @@
+a = 2
+b = 3
+print(a&b)
+
+print(a|b)
+
+print(~a)
+print(a^b)
+
+a = 17
+print(17>>1)
+print(17>>2)
+
+print(19<<1)
+print(21<<2)
+
+#There are no pre or post increment operators in python. 
